ca711a135d3b20d324a61180578b30cfcdd6e78d
suraj7076 <surajmaity981@gmail.com>
2022-08-30 09:35:09 +0530

    DSA

--- src/BinarySearch/BinarySearch.java
+++ src/BinarySearch/BinarySearch.java
@@ -1,5 +1,49 @@
 package BinarySearch;
 
 public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 60, 110, 120, 130, 170};
+        //System.out.println(binarySearchIteration(arr, 65));
+        System.out.println(binarySearchRecursion(arr, 0,arr.length-1, 460));
+    }
+    static boolean binarySearchRecursion(int[] arr, int start, int end, int target){
 
+
+        while (start<=end){
+            int mid = start + (end - start)/2;
+
+
+            if(arr[mid] > target){
+                return binarySearchRecursion(arr, start, mid-1, target);
+            }
+            else if (arr[mid] < target){
+                return binarySearchRecursion(arr, mid + 1, end, target);
+            }
+            else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+    static boolean binarySearchIteration(int[] arr, int target){
+        int start = 0;
+        int end = arr.length-1;
+
+        while (start<=end){
+            int mid = start + (end - start)/2;
+
+            if(arr[mid] > target){
+                end = mid - 1;
+            }
+            else if(arr[mid] < target){
+                start = mid + 1;
+            }
+            else{
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
