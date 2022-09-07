86f5225a4e284404085ad70c23439f690b6b9175
suraj7076 <surajmaity981@gmail.com>
2022-09-07 10:47:08 +0530

    DSA

--- src/BinaryTree/BinaryTree.java
+++ src/BinaryTree/BinaryTree.java
@@ -90,9 +90,40 @@ public class BinaryTree {
             }
         }
     }
+    public static int countNodes(Node root){
+        if (root == null) return 0;
+
+        return 1+ countNodes(root.left) + countNodes(root.right);
+    }
+    public static int sumOfNodes(Node root){
+        if (root == null) return 0;
+
+        return root.val + sumOfNodes(root.left) + sumOfNodes(root.right);
+    }
+    public static int height(Node root){
+        if (root == null) return 0;
+
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+    public static void diameter(Node root, int[] diameter){
+        if (root == null) return;
+
+        Integer leftHeight = height(root.left);
+        Integer rightHeight = height(root.right);
+        diameter[0] = Math.max(diameter[0], (leftHeight + rightHeight));
+
+        diameter(root.left, diameter);
+        diameter(root.right, diameter);
+    }
     public static void main(String[] args) {
         int[] nodes = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         Node root = BTree.buildTree(nodes);
         levelOrder(root);
+        int[] diameter = new int[1];
+        diameter(root, diameter);
+        System.out.println(diameter[0]);
     }
 }
