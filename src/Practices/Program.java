4d8613a9eb07014f339aab5f471e7fd95248c80d
suraj7076 <surajmaity981@gmail.com>
2022-09-23 12:08:18 +0530

    DSA

--- src/Practices/Program.java
+++ src/Practices/Program.java
@@ -1,8 +1,7 @@
 package Practices;
 
+import java.util.Stack;
+
 public class Program {
-    public static void main(String[] args) {
-        int k = -1/2;
-        System.out.println(k);
-    }
+
 }
