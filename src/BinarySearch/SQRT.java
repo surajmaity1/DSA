5f08d1d1c08a8e891c6d75ea8213681c276e0a05
suraj7076 <surajmaity981@gmail.com>
2022-10-10 11:08:28 +0530

    DSA

--- src/BinarySearch/SQRT.java
+++ src/BinarySearch/SQRT.java
@@ -3,16 +3,35 @@ package BinarySearch;
 import java.util.Scanner;
 
 public class SQRT {
-    // find square root using binary search
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
-        int decimalPlaces = 3;
 
         // power calculation using binary search
-        System.out.println(pcubs(number, decimalPlaces));
+        System.out.println(mySqrt(number));
     }
-    static double pcubs(int n, int p){
-        return 0.0;
+
+    static int mySqrt(int x) {
+        if(x==0 || x== 1){return x;}
+
+        int start = 1;
+        int end = x;
+        int ans = -1;
+
+        while(start <= end){
+            int mid = (start + ((end - start)/2));
+
+            if(mid > x/mid){
+                end = mid - 1;
+            }
+            else if(mid < x/mid){
+                start = mid + 1;
+                ans = mid;
+            }
+            else{
+                return mid;
+            }
+        }
+        return ans;
     }
 }
