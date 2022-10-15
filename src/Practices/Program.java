12197437be356cdb9f4f11437454ee4190954ab6
suraj7076 <surajmaity981@gmail.com>
2022-10-15 12:45:16 +0530

    DSA

--- src/Practices/Program.java
+++ src/Practices/Program.java
@@ -1,25 +1,2 @@
 package Practices;
 
-import java.util.Scanner;
-import java.util.Stack;
-
-public class Program {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        System.out.println(notChanged(str));
-    }
-    static int notChanged(String str){
-        StringBuilder sb = new StringBuilder(str);
-        sb.reverse();
-        String res = new String(sb);
-        int n = str.length();
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (str.charAt(i) == res.charAt(i)){
-                count++;
-            }
-        }
-        return count;
-    }
-}
